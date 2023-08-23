3efc8577c162da8cf56d93e086ae0ad295def8ae
flexxdarker <ksenzhakmax@gmail.com>
2023-08-23 20:51:23 +0300

    start of homework

---
+++ HM18.08.2023/Entities/Airplanes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM18._08._2023.Entities
+{
+    internal class Airplanes
+    {
+    }
+}
